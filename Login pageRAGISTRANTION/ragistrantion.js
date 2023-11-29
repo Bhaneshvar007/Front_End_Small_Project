0d3ec01fbbc192b61f1e7dbdb71f55f0ebd75eb8
Bhaneshvar kshirsagar <145997974+BhaneshvarKshirsagar@users.noreply.github.com>
2023-11-29 22:05:32 +0530

    Add files via upload

---
+++ Login pageRAGISTRANTION/ragistrantion.js
@@ -0,0 +1,6 @@
+let ragisterText = document.querySelector("#ragister");
+let bottom = document.querySelector(".bottom");
+
+ragisterText.addEventListener("click", () => {
+    bottom.classList.add("active")
+});
